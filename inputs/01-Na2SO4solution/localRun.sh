6cb70e685e400a30b0ba12635477290f9646bb04
Simon Gravelle <sgravelle@icp.uni-stuttgart.de>
2022-08-10 16:08:23 +0200

    Improved tutorial 1

--- inputs/01-Na2SO4solution/localRun.sh
+++ inputs/01-Na2SO4solution/localRun.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-gmx grompp -f input/min.mdp -c conf.gro -p topol.top -o min -pp min
+gmx grompp -f input/min.mdp -c conf.gro -p topol.top -o min -pp min -po min
 gmx mdrun -v -deffnm min
 
-gmx grompp -f input/nvt.mdp -c min.gro -p topol.top -o nvt -pp nvt
+gmx grompp -f input/nvt.mdp -c min.gro -p topol.top -o nvt -pp nvt -po nvt
 gmx mdrun -v -deffnm nvt
 
-gmx grompp -f input/npt.mdp -c nvt.gro -p topol.top -o npt -pp npt
+gmx grompp -f input/npt.mdp -c nvt.gro -p topol.top -o npt -pp npt -po npt
 gmx mdrun -v -deffnm npt
 
-gmx grompp -f input/run.mdp -c npt.gro -p topol.top -o run -pp run
-gmx mdrun -v -deffnm run
+gmx grompp -f input/pro.mdp -c npt.gro -p topol.top -o pro -pp pro -po pro
+gmx mdrun -v -deffnm pro
