279e87da288a5513c57fffb3630737ef2249bb97
Simon Gravelle <sgravelle@icp.uni-stuttgart.de>
2022-08-22 19:52:29 +0200

    terminated tutorial 3

--- inputs/03-EthanolAdsorption/adsorption/icprun.sh
+++ inputs/03-EthanolAdsorption/adsorption/icprun.sh
@@ -3,7 +3,7 @@
 set -e
 
 for ((i = 0 ; i < 30 ; i++)); do
-	x=$(echo "0.13*$(($i+1))" | bc);
+	x=$(echo "0.065*$(($i+1))" | bc);
 	sed 's/to_be_replaced/'$x'/g' inputs/min.mdp > min.mdp
 	/work/sgravelle/Softwares/gromacs-install/bin/gmx grompp -f min.mdp -c ../preparation/nvt_1ns.gro -p topol.top -o min.$i -pp min.$i -po min.$i -maxwarn 1 -n index.ndx
 	/work/sgravelle/Softwares/gromacs-install/bin/gmx mdrun -v -deffnm min.$i
@@ -12,9 +12,9 @@ for ((i = 0 ; i < 30 ; i++)); do
 	/work/sgravelle/Softwares/gromacs-install/bin/gmx grompp -f nvt.mdp -c min.$i.gro -p topol.top -o nvt.$i -pp nvt.$i -po nvt.$i -maxwarn 1 -n index.ndx
 	/work/sgravelle/Softwares/gromacs-install/bin/gmx mdrun -v -deffnm nvt.$i
 	
-	sed 's/to_be_replaced/'$x'/g' inputs/pull.mdp > pull.mdp
-	/work/sgravelle/Softwares/gromacs-install/bin/gmx grompp -f pull.mdp -c nvt.$i.gro -p topol.top -o pull.$i -pp pull.$i -po pull.$i -maxwarn 1 -n index.ndx
-	/work/sgravelle/Softwares/gromacs-install/bin/gmx mdrun -v -deffnm pull.$i
+	sed 's/to_be_replaced/'$x'/g' inputs/pro.mdp > pro.mdp
+	/work/sgravelle/Softwares/gromacs-install/bin/gmx grompp -f pro.mdp -c nvt.$i.gro -p topol.top -o pro.$i -pp pro.$i -po pro.$i -maxwarn 1 -n index.ndx
+	/work/sgravelle/Softwares/gromacs-install/bin/gmx mdrun -v -deffnm pro.$i
 done
 
 
