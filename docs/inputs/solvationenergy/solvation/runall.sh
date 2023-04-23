c30060c5b4ccb61c7e4eb2d9c519311676b05d9c
Simon Gravelle <simon.gravelle@live.fr>
2023-04-23 21:26:42 +0200

    added preparation

--- docs/inputs/solvationenergy/solvation/runall.sh
+++ docs/inputs/solvationenergy/solvation/runall.sh
@@ -2,148 +2,148 @@
 
 cd lambdastate0
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate1
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate2
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate3
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate4
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate5
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate6
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate7
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate8
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate9
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate10
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate11
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate12
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate13
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate14
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate15
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate16
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate17
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate18
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate19
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
 cd lambdastate20
 gmx grompp -f inputs/npt_bis.mdp -c preparedstate.gro -p topol.top -o npt_bis -pp npt_bis -po npt_bis -maxwarn 1
-gmx mdrun -v -deffnm npt_bis -rdd 2
+gmx mdrun -v -deffnm npt_bis -nt 4
 gmx grompp -f inputs/pro.mdp -c npt_bis.gro -p topol.top -o pro -pp pro -po pro -maxwarn 1
-gmx mdrun -v -deffnm pro -rdd 2
+gmx mdrun -v -deffnm pro -nt 4
 cd ..
 
